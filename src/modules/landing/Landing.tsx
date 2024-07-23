fc4ce85561f4435bb2016cd0df42a14653971102
Shrey Koradia <67724256+shreykoradia@users.noreply.github.com>
2024-07-23 06:45:41 +0000

    addition of padding on mobile devices

--- src/modules/landing/Landing.tsx
+++ src/modules/landing/Landing.tsx
@@ -8,7 +8,7 @@ import Footer from "../footer";
 
 const Landing = () => {
   return (
-    <section className="flex flex-col justify-start items-start flex-nowrap gap-0 min-h-screen pl-[3.75rem] pb-[7.5rem]">
+    <section className="flex flex-col justify-start items-start flex-nowrap gap-0 min-h-screen pl-[3.75rem] pb-[7.5rem] maxMd:p-4">
       <div className="max-w-[37.5rem] w-full pt-[5rem] flex flex-col gap-16">
         <Hero />
         <Projects />
