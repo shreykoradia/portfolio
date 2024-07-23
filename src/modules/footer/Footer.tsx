432d5573c98f148221c40a5b74b881bf2732f7f6
Shrey Koradia <67724256+shreykoradia@users.noreply.github.com>
2024-07-23 07:11:27 +0000

    added mail

--- src/modules/footer/Footer.tsx
+++ src/modules/footer/Footer.tsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <section>
       <p>Maximizing My Potential Energy!</p>
+      <p className="text-grey">For work mail me @t <a href="mailto:device.shreykoradia@gmail.com" className="text-blue-400 hover:underline">device.shreykoradia@gmail.com</a></p>
     </section>
   );
 };
